2535a26864cce18ffb7df72f30f84a0b6bd2ee9a
unknown <akash.manna@webskitters.com>
2022-09-04 14:06:45 +0530

    complete imdb clone app short project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/MovieList';
+import Movie from './pages/movieDetails/Movie';
 
 function App() {
   return (
     <div className="App">
-      <h1>Akash Manna</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path='movie/:id' element={<Movie />}></Route>
+          <Route path='/*' element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
